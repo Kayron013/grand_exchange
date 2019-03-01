9295cbc92c75b28f4340be8f95e3a55081786d34
jielan <lanjie3458@gmail.com>
2019-03-01 09:27:39 -0500

    add placeholder for credential

--- src/components/ExchangeForm/ExchangeForm.js
+++ src/components/ExchangeForm/ExchangeForm.js
@@ -5,12 +5,12 @@ import './ExchangeForm.scss';
 
 export class ExchangeForm extends Component {
     state = {
-        server: '',
-        exchange: '',
+        server: '10.50.78.90',
+        exchange: 'slow_segment_exchange',
         routing_key: '',
         is_durable: false,
-        username: '',
-        password: '',
+        username: 'sire',
+        password: 'sire001a',
         show_password: false,
         //record if the all necessary textfiled has been click
         touched: {
