d2450e52d3a7c9031f7b3a8046dfe4b918a33942
Unknown <kayron013@gmail.com>
2019-04-24 13:17:02 -0400

    Added max-width

--- src/App.js
+++ src/App.js
@@ -106,7 +106,7 @@ export class App extends Component {
 
     renderExchanges = () => {
         return this.state.exchanges.map((exchange, i) => (
-            <Grid item xs={6} md={4} key={i}>
+            <Grid item xs={12} md={4} key={i}>
                 <Exchange {...exchange} closeHandler={this.removeExchange}/>
             </Grid>
         ))
